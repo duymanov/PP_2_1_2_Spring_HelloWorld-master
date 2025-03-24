4d42cf17b9b587bd064e201ee55aca2f19b0ee70
Springbean <duymanov@internet.ru>
2025-03-24 22:45:54 +0300

    123

---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+class Cat {
+    private String message;
+
+    public Cat(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
